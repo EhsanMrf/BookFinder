c7ad54e15a1345a4e5a70555d3da11d70cabffb8
EhsanMrf <developermrf@gmail.com>
2024-09-27 13:31:22 +0330

    test done

---
+++ BookService/BookService/Domain.Model.Test/Author/AuthorTest.cs
@@ -0,0 +1,41 @@
+﻿
+using Domain.Model.Model.Author;
+using FluentAssertions;
+
+namespace Domain.Model.Test.Author;
+
+public class AuthorTest
+{
+
+    [Fact]
+    public void should_throw_exception_if_name_null()
+    {
+        var courser = () =>
+        {
+            new Model.Author.Author(null);
+        };
+
+        courser.Should().Throw<AuthorNameNullException>();
+    }
+
+    [Fact]
+    public void should_throw_exception_if_name_less_two()
+    {
+        var courser = () =>
+        {
+            new Model.Author.Author("A");
+        };
+
+        courser.Should().Throw<AuthorNameLengthException>();
+    }
+
+    [Fact]
+    public void should_instance_with_out_exception()
+    {
+        var courser = () =>
+        {
+            var author = new Model.Author.Author(nameof(Model.Author.Author));
+        };
+        courser.Should().NotThrow();
+    }
+}
---
+++ BookService/BookService/Domain.Model.Test/Book/BookTitle.cs
@@ -0,0 +1,50 @@
+﻿using Domain.Model.Model.Book;
+using FluentAssertions;
+
+namespace Domain.Model.Test.Book;
+
+public class BookTitleTest
+{
+    [Fact]
+    public void should_throw_exception_if_title_null()
+    {
+        var courser = () =>
+        {
+            BookTitle.CreateInstance(null);
+        };
+
+        courser.Should().Throw<BookTitleNullException>();
+    }
+    [Fact]
+    public void should_throw_exception_if_title_less_two()
+    {
+        var courser = () =>
+        {
+            BookTitle.CreateInstance("A");
+        };
+
+        courser.Should().Throw<BookTitleLengthException>();
+    }
+
+    [Fact]
+    public void should_throw_exception_if_title_equalto_two()
+    {
+        var courser = () =>
+        {
+            BookTitle.CreateInstance("Ab");
+        };
+
+        courser.Should().Throw<BookTitleLengthException>();
+    }
+
+    [Fact]
+    public void should_instance_with_out_exception()
+    {
+        var courser = () =>
+        {
+            BookTitle.CreateInstance("Book Title");
+        };
+
+        courser.Should().NotThrow();
+    }
+}
