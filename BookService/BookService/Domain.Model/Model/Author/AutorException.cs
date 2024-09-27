7e11724a38638016d3bf336f128916fa36c236e3
EhsanMrf <developermrf@gmail.com>
2024-09-27 13:49:42 +0330

    Locazation and exception res

--- BookService/BookService/Domain.Model/Model/Author/AutorException.cs
+++ BookService/BookService/Domain.Model/Model/Author/AutorException.cs
@@ -1,6 +1,7 @@
 ﻿using Framework.Exception;
+using Infrastructure.Locazation.Author;
 
 namespace Domain.Model.Model.Author;
 
-public class AuthorNameNullException() : BaseException("AuthorResource.AuthorNameNull");
-public class AuthorNameLengthException() : BaseException("AuthorResource.AuthorNameLength");
+public class AuthorNameNullException() : BaseException(AuthorResource.AuthorNameNull);
+public class AuthorNameLengthException() : BaseException(AuthorResource.AuthorNameLength);
--- BookService/BookService/Domain.Model/Model/Book/BookException.cs
+++ BookService/BookService/Domain.Model/Model/Book/BookException.cs
@@ -1,9 +1,10 @@
 ﻿using Framework.Exception;
+using Infrastructure.Locazation.Book;
 
 namespace Domain.Model.Model.Book;
 
-public class BookTitleNullException() : BaseException("BookResource.BookTitleNull");
-public class BookTitleLengthException() : BaseException("BookResource.BookTitleLength");
-public class BookTitleInvalidObjectException() : BaseException("BookResource.BookTitleInvalidObject");
-public class BookPublishYearException() : BaseException("BookResource.BookPublishYear");
-public class BookAuthorIdInvalidException() : BaseException("BookResource.BookAuthorIdInvalid");
+public class BookTitleNullException() : BaseException(BookResource.BookTitleNull);
+public class BookTitleLengthException() : BaseException(BookResource.BookTitleLength);
+public class BookTitleInvalidObjectException() : BaseException(BookResource.BookTitleInvalidObject);
+public class BookPublishYearException() : BaseException(BookResource.BookPublishYear);
+public class BookAuthorIdInvalidException() : BaseException(BookResource.BookAuthorIdInvalid);
