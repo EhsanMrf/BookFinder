0adbd3020b1be602339a7b4a07ea661a9705ad35
EhsanMrf <developermrf@gmail.com>
2024-09-27 12:36:23 +0330

    init Framework

---
+++ BookService/BookService/Framework/Extensions/ConditionExtension.cs
@@ -0,0 +1,11 @@
+﻿using Framework.Exception;
+
+namespace Framework.Extensions;
+
+public static class ConditionExtension
+{
+    public static void ThrowIfNull<T>(this T @object, BaseException exception)
+    {
+        if (@object==null) throw exception;
+    }
+}
